ca05ba319558c621cc34782d7a3ad6977add8386
OllyTeam <imanishimweolyy@gmail.com>
2019-07-09 13:17:20 +0200

    this is a ussd commit

--- server.js
+++ server.js
@@ -9,40 +9,135 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('*', (req, res) => {
-  res.send('This is a Game prepaid USSD App')
+  res.send('CON This is a Game prepaid USSD App')
 })
 
 app.post('*', (req, res) => {
   let {sessionId, serviceCode, phoneNumber, text} = req.body
   if (text == '') {
     // This is the first request. Note how we start the response with CON
-    let response = `CON What would you want to check
-    1. My Account
-    2. My phone number`
+    let response = `CON Welcome to GAME-PREPAID choose a suitable game
+    1. Mukura VS APR
+    2. Rayon VS Kiyovu
+    3. Police VS Marine`
     res.send(response)
   } else if (text == '1') {
     // Business logic for first level response
-    let response = `CON Choose account information you want to view
-    1. Account number
-    2. Account balance`
+    let response = `CON Mukura VS APR 
+    Match Time:15:30
+    Place: Huye Stadium
+    Choose a price
+    1. V-VIP:10 000 rwf
+    2. VIP: 5000 rwf
+    3. REST: 3000 rwf`
     res.send(response)
   } else if (text == '2') {
     // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`
+    let response = `CON Rayon VS Kiyovu 
+    Match Time:18:30
+    Place: Amahoro Stadium
+    Choose a price
+    1. V-VIP:20 000 rwf
+    2. VIP: 5000 rwf
+    3. REST: 1000 rwf`
     res.send(response)
-  } else if (text == '1*1') {
+  } else if (text == '3') {
     // Business logic for first level response
-    let accountNumber = 'ACC1001'
+    let response = `CON Police VS Marine 
+    Match Time:16:30
+    Place: Umuganda Stadium
+    Choose a price
+    1. V-VIP:5 000 rwf
+    2. VIP: 3000 rwf
+    3. REST: 1000 rwf`
+    res.send(response)
+  }
+  else if (text == '1*1') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
     // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON Choose the payment you use 
+    
+    1. Mobile Money
+    2. Aitel/Tigo
+    3. Bank`
     res.send(response)
   } else if (text == '1*2') {
-    // This is a second level response where the user selected 1 in the first instance
-    let balance = 'NGN 10,000'
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
     // This is a terminal request. Note how we start the response with END
-    let response = `END Your balance is ${balance}`
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON Choose the payment you use 
+    
+    1. Mobile Money
+    2. Aitel/Tigo
+    3. Bank`
     res.send(response)
-  } else {
+  } 
+  else if (text == '1*3') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
+    // This is a terminal request. Note how we start the response with END
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON Choose the payment you use 
+    
+    1. Mobile Money
+    2. Aitel/Tigo
+    3. Bank`
+    res.send(response)
+  }
+  else if (text == '1*1*1') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
+    // This is a terminal request. Note how we start the response with END
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON MoMo Pay, Give your Phone number
+    1. Confirm paying with MoMo
+    2. BAck
+   `
+    res.send(response)
+  }
+  else if (text == '1*1*1') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
+    // This is a terminal request. Note how we start the response with END
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON MoMo Pay, Give your Password to confirm`
+    res.send(response)
+  }
+  else if (text == '1*1') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
+    // This is a terminal request. Note how we start the response with END
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON Choose the payment you use 
+    
+    1. Mobile Money
+    2. Aitel/Tigo
+    3. Bank`
+    res.send(response)
+  }
+  else if (text == '1*1') {
+    // Business logic for first level response
+    // let accountNumber = 'ACC1001'
+    // This is a terminal request. Note how we start the response with END
+    // let response = `END Your account number is ${accountNumber}`
+
+    let response = `CON Choose the payment you use 
+    
+    1. Mobile Money
+    2. Aitel/Tigo
+    3. Bank`
+    res.send(response)
+  }
+  else {
     res.status(400).send('Bad request!')
   }
 })
